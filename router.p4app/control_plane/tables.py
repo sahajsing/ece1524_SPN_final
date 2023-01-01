077c32eaed58e89451538fc3d7b497ead06fb108
sahasing <sahaj.singh@outlook.com>
2022-12-31 20:26:18 -0500

    removed pass

--- router.p4app/control_plane/tables.py
+++ router.p4app/control_plane/tables.py
@@ -226,9 +226,9 @@ class Tables_populator():
         E.g., self.api.table_cam_add_entry(TABLE_NAME, match_fields={...}, action_name='...', action_params={...})
         """
         for iface in ifaces:
-            # self.api.table_cam_add_entry(table_name='MyIngress.local_ip_table',keys={'p.ip.dstAddr':iface.ip}, action_name='MyIngress.send_to_cpu',action_data={'DIG_CODE':DIG_LOCAL_IP})
-            self.api.table_cam_add_entry(table_name='MyIngress.local_ip_table',match_fields={'p.ip.dstAddr':iface.ip}, action_name='MyIngress.ipv4_forward',action_params={'port':iface.port,'next':iface.ip})
-        pass
+            self.api.table_cam_add_entry(table_name='MyIngress.local_ip_table',match_fields={'p.ip.dstAddr':iface.ip},\
+                 action_name='MyIngress.send_to_cpu',action_params={'DIG_CODE':DIG_LOCAL_IP})
+        #pass
 
 
     """
@@ -243,7 +243,7 @@ class Tables_populator():
         E.g., self.api.table_lpm_load_dataset(RTABLE_NAME, rtable)
         """
         self.api.table_lpm_load_dataset(RTABLE_NAME, rtable)
-        pass
+        #pass
 
     """
     Populates relevant p4 tables given a list of arp cache entries
@@ -258,4 +258,5 @@ class Tables_populator():
         """
         for arp in arp_cache:
             # self.api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, keys={'p.arp.dstIP':arp[0]},action_name='MyIngress.arp_match',action_data={'dstAddr':arp[1]})
-            self.api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, match_fields={'p.arp.dstIP':arp[0]},action_name='MyIngress.arp_match',action_params={'dstAddr':arp[1]})
+            self.api.table_cam_add_entry(ARP_CACHE_TABLE_NAME, match_fields={'p.arp.dstIP':arp[0]},\
+                action_name='MyIngress.arp_match',action_data={'dstAddr':arp[1]})
